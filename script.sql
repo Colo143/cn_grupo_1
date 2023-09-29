717b65c09d8f0fcaac241ee57cc9c9145fc2e95d
Exequiel <coloexe143@gmail.com>
2023-09-29 03:12:49 -0300

    Script SQL actualizado

--- script.sql
+++ script.sql
@@ -1,12 +1,12 @@
 create database discord;
 
 create table discord.user(
-	user_id int not null auto_increment primary key,
+    user_id int not null auto_increment primary key,
     nombre varchar(30) not null,
     apellido varchar(30) not null,
     contrasenia varchar(20) not null,
     fecha_nac varchar(30) not null,
-    usuario varchar(30) not null,
+    user_name varchar(30) not null,
     email varchar(30) not null
 );
 
